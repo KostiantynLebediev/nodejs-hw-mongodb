4c259a8c4dc04b86d5323a726c4e712f658c39c9
KostiantynLebediev <lebedievkostiantyn@gmail.com>
2025-01-27 18:46:34 +0000

    hw 4 done

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,5 @@
 import createHttpError from "http-errors";
+
 import {
   createContact,
   getAllContacts,
@@ -9,11 +10,13 @@ import {
 import { parsePaginationParams } from "../utils/parsePaginationParams.js";
 import { parseSortParams } from "../utils/parseSortParams.js";
 import { parseFilterParams } from "../utils/parseFilterParams.js";
-export const getContactsController = async (req, res) => {
+import { CONTACT_KEYS } from "../db/models/contacts.js";
 
-  const { page,perPage }= parsePaginationParams(req.query);
-  const { sortBy, sortOrder }= parseSortParams(req.query);
+export const getContactsController = async (req, res) => {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query, CONTACT_KEYS);
   const filter = parseFilterParams(req.query);
+
   const contacts = await getAllContacts({
     page,
     perPage,
@@ -27,58 +30,70 @@ export const getContactsController = async (req, res) => {
     data: contacts,
   });
 };
-export const getContactByIdController = async (req, res, next) => {
+
+export const getContactByIdController = async (req, res) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
+
   if (!contact) {
     throw createHttpError(404, "Contact not found");
   }
+
   res.status(200).json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
   });
 };
+
 export const createContactController = async (req, res) => {
   const contact = await createContact(req.body);
+
   res.status(201).json({
     status: 201,
-    message: "Succesfully created a contact!",
+    message: "Successfully created a contact!",
     data: contact,
   });
 };
-export const deleteContactController = async (req, res, next) => {
+
+export const deleteContactController = async (req, res) => {
   const { contactId } = req.params;
   const contact = await deleteContact(contactId);
+
   if (!contact) {
-    next(createHttpError(404, "Contact not found"));
-    return;
+    throw createHttpError(404, "Contact not found");
   }
+
   res.status(204).send();
 };
-export const upsertContactController = async (req, res, next) => {
+
+export const upsertContactController = async (req, res) => {
   const { contactId } = req.params;
   const result = await updateContact(contactId, req.body, {
     upsert: true,
   });
+
   if (!result) {
-    next(createHttpError(404, "Contact not found"));
-    return;
+    throw createHttpError(404, "Contact not found");
   }
+
   const status = result.isNew ? 201 : 200;
+
   res.status(status).json({
     status: status,
     message: "Successfully upserted a contact!",
     data: result.contact,
   });
 };
-export const patchContactController = async (req, res, next) => {
+
+export const patchContactController = async (req, res) => {
   const { contactId } = req.params;
   const result = await updateContact(contactId, req.body);
+
   if (!result) {
-    next(createHttpError(404, "Contact not found"));
-    return;
+    throw createHttpError(404, "Contact not found");
   }
+
   res.status(200).json({
     status: 200,
     message: "Successfully patched a contact!",
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,59 +1,19 @@
-import Joi from 'joi';
+import Joi from "joi";
+
+import { CONTACT_TYPES } from "../contacts/index.js";
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'Name should be a string',
-    'string.min': 'Name should have at least 3 characters',
-    'string.max': 'Name should have at most 20 characters',
-    'any.required': 'Name is required',
-  }),
-  phoneNumber: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'Phonenumber should be a string',
-    'string.min': 'Phonenumber should have at least 3 characters',
-    'string.max': 'Phonenumber should have at most 20 characters',
-    'any.required': 'Phonenumber is required',
-  }),
-  email: Joi.string().min(3).max(20).messages({
-    'string.base': 'Email should be a string',
-    'string.min': 'Email should have at least 3 characters',
-    'string.max': 'Email should have at most 20 characters',
-  }),
+  name: Joi.string().min(3).max(30).required(),
+  phoneNumber: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-    .valid('work', 'home', 'personal')
-    .default('personal')
-    .required()
-    .messages({
-      'string.base': 'Contact type should be a string',
-      'any.only':
-        'Contact type must be one of the following: work, home, or personal',
-      'any.required': 'Contact type is required',
-    }),
+  contactType: Joi.string().valid(...CONTACT_TYPES).required(),
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).messages({
-    'string.base': 'Name should be a string',
-    'string.min': 'Name should have at least 3 characters',
-    'string.max': 'Name should have at most 20 characters',
-  }),
-  phoneNumber: Joi.string().min(3).max(20).messages({
-    'string.base': 'Phonenumber should be a string',
-    'string.min': 'Phonenumber should have at least 3 characters',
-    'string.max': 'Phonenumber should have at most 20 characters',
-  }),
-  email: Joi.string().min(3).max(20).messages({
-    'string.base': 'Email should be a string',
-    'string.min': 'Email should have at least 3 characters',
-    'string.max': 'Email should have at most 20 characters',
-  }),
+  name: Joi.string().min(3).max(30),
+  phoneNumber: Joi.string().min(3).max(30),
+  email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-    .valid('work', 'home', 'personal')
-    .default('personal')
-    .messages({
-      'string.base': 'Contact type should be a string',
-      'any.only':
-        'Contact type must be one of the following: work, home, or personal',
-    }),
+  contactType: Joi.string().valid(...CONTACT_TYPES),
 });
